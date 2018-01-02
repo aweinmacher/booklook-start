4f05f6b66fe48c55897e3f15824149847bfbfc6d
Anna W <aweinmacher@gmail.com>
2018-01-02 13:08:57 +0200

    search by name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var fetch = function () {
-  var isbn = $("#isbn").val();
-  var buildUrl = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}` // 0439023521
+  var title = $("#title").val();
+  var buildUrl = `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}` // isbn:0439023521
   // AJAX
   $.ajax({
     method: "GET",
@@ -11,15 +11,11 @@ var fetch = function () {
       // HANDLEBARS
       var source = $("#book-template").html();
       var template = Handlebars.compile(source);
-      var newHTML = template(
-        {
-          title: data.items[0].volumeInfo.title,
-          description: data.items[0].volumeInfo.description,
-          author: data.items[0].volumeInfo.authors,
-          imageLink: data.items[0].volumeInfo.imageLinks.smallThumbnail
-        }
-      );
-      $(".book").append(newHTML); // END OF HANDLEBARS
+      var newHTML = template(data);
+      $(".book").append(newHTML); 
+      
+      
+      // END OF HANDLEBARS
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
