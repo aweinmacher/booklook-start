fd2a327c12d46f406078a78f601db2c35fd8d32d
Anna W <aweinmacher@gmail.com>
2018-01-02 09:57:23 +0200

    handlebars

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-var fetch = function() {
-    $.ajax({
+// AJAX
+var fetch = function () {
+  $.ajax({
     method: "GET",
     url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
-    success: function(data) {
+    success: function (data) {
       console.log(data);
-      // var bookInfo = {
-      //   title: data.items[0].volumeInfo.title,
-      //   description: data.items[0].volumeInfo.description,
-      //   author: data.items[0].volumeInfo.authors[0],
-      //   imageLink: data.items[0].volumeInfo.imageLinks.smallThumbnail        
-      // };
-      var showBook = function() {
-        $(".book").empty();
-        $(".book").append(`<h1>${data.items[0].volumeInfo.title}</h1>
-                          <p>${data.items[0].volumeInfo.description}<p>
-                          <h3>Written by: ${data.items[0].volumeInfo.authors[0]}</h3>
-                          <img src="${data.items[0].volumeInfo.imageLinks.smallThumbnail}">`);
-      };
-      showBook();
+      $(".book").empty();
+      // HANDLEBARS
+      var source = $("#book-template").html();
+      var template = Handlebars.compile(source);
+      var newHTML = template(
+        {
+          title: data.items[0].volumeInfo.title,
+          description: data.items[0].volumeInfo.description,
+          author: data.items[0].volumeInfo.authors[0],
+          imageLink: data.items[0].volumeInfo.imageLinks.smallThumbnail
+        }
+      );
+      $(".book").append(newHTML); // END OF HANDLEBARS
     },
-    error: function(jqXHR, textStatus, errorThrown) {
+    error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
-  }); 
+  });
 };
 
 $(".search-book").on("click", fetch);
